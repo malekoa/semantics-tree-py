f7f545cc696ac7eb65124990e628c2ea20f7f610
mazadegan <azadeganmalek@gmail.com>
2022-11-16 00:13:06 -0500

    backtracking algorithm implemented

---
+++ README.md
@@ -0,0 +1,30 @@
+# semantics-tree
+
+## Main Idea
+
+This project is meant to evaluate the semantic values of sentences by following
+the semantics model theory rules outlined in LING 371.
+
+Essentially, a user of this software should provide a set of rewrite rules, a
+sentence, and a model that maps each constituent in all generatable syntax trees
+to a semantic value.
+
+The software should produce all valid syntax trees by using the rewrite rules.
+Then, it should apply the model's rules to each constituent node in the valid
+syntax trees it generates to evaluate the semantic values of each sentence.
+
+### Project Requirements
+
+- [x] Generate syntax trees following a set of rewrite rules.
+- [ ] Allow for user-defined rewrite rules.
+- [ ] Generate ALL valid syntax trees, not just the first valid one.
+- [ ] Increase algorithm efficiency.
+- [ ] Improve robustness of algorithm.
+- [ ] Apply model rules to generated syntax trees.
+- [ ] Figure out if there is a way to allow greater freedom for user defined models.
+
+### Current State
+
+- Project currently generates syntax trees using a hardcoded ruleset and a quite
+inefficient backtracking algorithm that I quickly put together.
+- Project currently does not yet implement anything related to the model.
---
+++ main.py
@@ -0,0 +1,118 @@
+from typing import List, Optional, Union
+from copy import deepcopy
+
+
+rules = {
+    'S coord S': 'S',
+    'NP VP': 'S',
+    'NP coord NP': 'NP',
+    'PN': 'NP',
+    'Det N': 'NP',
+    'VP Adv': 'VP',
+    'Adv VP': 'VP',
+    'VP coord VP': 'VP',
+    'V_T NP': 'VP',
+    'V_I': 'VP',
+    'supported': 'V_T',
+    'criticized': 'V_T',
+    'admired': 'V_T',
+    'yawned': 'V_I',
+    'slept': 'V_I',
+    'Jojo': 'PN',
+    'Rosa': 'PN',
+    'and': 'coord',
+    'or': 'coord',
+    'the': 'Det',
+    'a': 'Det',
+    'linguist': 'N',
+    'psychologist': 'N',
+    'loudly': 'Adv',
+    'enthusiastically': 'Adv',
+    'secretly': 'Adv',
+}
+
+"""
+Each state contains a list of constituents and a list of valid rewrite rules that 
+can be applied to them. If the list of valid rewrite rule reversals is of length 
+0 and len(constituents) != 1, the state is invalid and can be thrown away.
+"""
+class State:
+    class Node:
+        def __init__(self, title: str, children: Optional[List] = None) -> None:
+            self.title = title
+            self.children = children
+
+        def __repr__(self) -> str:
+            return f'<Node title="{self.title}" children="{self.children}">'
+
+        @classmethod
+        def traverse(cls, root) -> None:
+            if root.children:
+                for child in root.children:
+                    cls.traverse(child)
+            else:
+                print(root.title)
+
+
+    def __init__(self, sentence: Union[str, List[Node]]) -> None:
+        if isinstance(sentence, str):
+            self.constituents = [self.Node(title=i) for i in sentence.split(" ")]
+        elif isinstance(sentence, list):
+            self.constituents = sentence
+        self.valid_rules = self.get_valid_rules()
+
+    def apply_rule(self):
+        if self.valid_rules:
+            i, j = self.valid_rules.pop()
+            rewritten = " ".join(node.title for node in self.constituents[i:j])
+            new_constituents = deepcopy(self.constituents)
+            new_constituents[i:j] = [self.Node(rules[rewritten], children=self.constituents[i:j])]
+            return State(new_constituents)
+
+    def has_valid_rules(self) -> bool:
+        return self.valid_rules != None and len(self.valid_rules) > 0
+
+    def __repr__(self) -> str:
+        return " ".join(node.title for node in self.constituents)
+    
+    """
+    Valid rewrite rule reversals can be of length 1 to 3. Therefore, all subsets
+    of lengths 1, 2, and 3 should be checked to see if they form a valid rewrite
+    rule reversal. The valid rules themselves are a list of tuples containing 
+    index ranges in self.constituents that form a valid rule reversal.
+    """
+    def get_valid_rules(self) -> Optional[List]:
+        valid_rules = []
+        for i in range(3): # There are rules of length 1, 2, and 3. This gets all of them
+            for j in range(i, len(self.constituents) + 1):
+                possible_rule = " ".join(node.title for node in self.constituents[j - (i + 1) : j])
+                print(f'possible_rule: {possible_rule}')
+                if possible_rule in rules:
+                    valid_rules.append((j - ( i + 1 ), j))
+        if len(valid_rules) > 0:
+            return valid_rules
+        return None
+
+valid_trees = []
+Z = [State("Rosa secretly admired the linguist and the psychologist and Jojo criticized Rosa")]
+
+while len(Z) > 0:
+    print(Z)
+    top = Z[-1]
+    if top.has_valid_rules():
+        new_state = top.apply_rule()
+        if new_state != None:
+            Z.append(new_state)
+            continue
+    else:
+        if len(top.constituents) == 1:
+            valid_trees.append(top.constituents[0])
+            break
+        Z.pop()
+
+if len(valid_trees) == 0:
+    print('found no valid trees')
+else:
+    root = (valid_trees[0])
+    State.Node.traverse(root)
+
